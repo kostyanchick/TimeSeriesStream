f773d3fbbacd806f2f2841730106d91e19515aa6
kostyanchick <yosipiv.yuriy@gmail.com>
2019-12-07 21:55:12 +0200

    fixed ports to link in config in ts_client

--- ts_client/service_api/config.py
+++ ts_client/service_api/config.py
@@ -3,8 +3,8 @@ import os
 APP_HOST = os.getenv('HOST', '0.0.0.0')
 APP_PORT = int(os.getenv('PORT', 8082))
 
-GENERATOR_HOST = os.getenv('HOST', '0.0.0.0')
-GENERATOR_PORT = int(os.getenv('PORT', 8081))
+GENERATOR_HOST = os.getenv('GENERATOR_HOST', '0.0.0.0')
+GENERATOR_PORT = int(os.getenv('GENERATOR_PORT', 8081))
 GENERATOR_URL = f'http://{GENERATOR_HOST}:{GENERATOR_PORT}/ws/numbers'
 
 ACCESS_LOG_FORMAT = " :: %r %s %T %t"
